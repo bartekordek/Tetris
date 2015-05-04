99fc8530cce8c25c909e67dad4c166f29574f1da
Kordek Bartlomiej <bartekordek@gmail.com>
2015-05-04 15:20:44 +0200

    Added initialization to main game loop class.

--- Tetris/main.cpp
+++ Tetris/main.cpp
@@ -2,11 +2,16 @@
 #include <FL/Fl_Box.H>
 #include <FL/Fl_Window.H>
 
+#include "Game.h"
+
 int main( void )
 {
+	CGame::Instance()->Initialize();
+
 	Fl_Window window( 300, 300, "OKNO" );
 	Fl_Box box( 0, 0 , 300, 300, "haj wrld" );
 	window.show();
 	Fl::run();
+	CGame::Instance()->Destroy();
 	return 0;
 }
