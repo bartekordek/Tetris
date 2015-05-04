49252ed44a03f20a428ed43d90195c62e597f411
Kordek Bartlomiej <bartekordek@gmail.com>
2015-05-04 15:20:21 +0200

    Changes to Slab constructors.

--- Tetris/inc/Slab.h
+++ Tetris/inc/Slab.h
@@ -2,18 +2,20 @@
 #define __CSLAB__
 
 #include "Picture.h"
+#include "Position.h"
 
 class CSlab
 {
 public:
 	CSlab( const CPicture& picture );
 	CSlab( const CSlab& slab );
+	CSlab( const unsigned int xPos, const unsigned int yPos );
 	void Draw();
 
 private:
 	bool m_hasBorder;
 	CPicture m_picture;
-
+	CPosition m_position;
 };
 
 #endif
--- Tetris/src/Slab.cpp
+++ Tetris/src/Slab.cpp
@@ -2,19 +2,30 @@
 
 CSlab::CSlab(const CPicture& picture): 
 	m_hasBorder( false ),
-	m_picture( picture )
+	m_picture( picture ),
+	m_position( CPosition() )
 {
 
 }
 
 CSlab::CSlab( const CSlab& slab ): 
 	m_hasBorder( slab.m_hasBorder ),
-	m_picture( slab.m_picture )
+	m_picture( slab.m_picture ),
+	m_position( CPosition() )
 {
 
 }
 
+CSlab::CSlab( const unsigned int xPos, const unsigned int yPos ):
+	m_hasBorder( false ),
+	m_picture( CPicture() ),
+	m_position( CPosition( xPos, yPos ) )
+{
+}
+
+
+
 void CSlab::Draw()
 {
-
+	//TODO
 }
