9b945a0b1b73c4b08a2fdad801731c1be62ab9b9
Kordek Bartlomiej <bartekordek@gmail.com>
2015-05-05 15:05:58 +0200

    Added fltk wrapper class.

--- Tetris/inc/FLTKWrapper.h
+++ Tetris/inc/FLTKWrapper.h
@@ -1,7 +1,29 @@
 #ifndef __FLTK_WRPR__
 #define __FLTK_WRPR__
 
+#include <FL/Fl.H>
+#include "MainGrid.h"
+#include <string>
 
+class FLTKWrapper
+{
+public:
+	static FLTKWrapper* Instance();
+	void InitWindow( const unsigned int xSize = 800, const unsigned ySize = 600 );
+	void ShowWindow();
+	void Display( const CMainGrid& grid );
+	void DisplayImage( const std::string& imagePath, const unsigned int xPos, const unsigned yPos );
+
+	virtual ~FLTKWrapper();
+
+private:
+	FLTKWrapper();
+	FLTKWrapper& operator=( const FLTKWrapper& wrapper );
+	FLTKWrapper( const FLTKWrapper& wrapper );
+	static FLTKWrapper* s_instance;
+
+	Fl_Window* m_window;
+};
 
 
 #endif
