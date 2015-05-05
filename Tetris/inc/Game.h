1b46de08c9e30e852dd2a8eef8581b676e7ec038
Kordek Bartlomiej <bartekordek@gmail.com>
2015-05-05 15:04:38 +0200

    Initialization and createWindow methods now have parameters. Added private methods, to set game grid.

--- Tetris/inc/Game.h
+++ Tetris/inc/Game.h
@@ -1,19 +1,26 @@
 #ifndef __GAME_Z__
 #define __GAME_Z__
 
+#include "MainGrid.h"
+
 class CGame
 {
 public:
 	static CGame* Instance();
 	static void Destroy();
 
-	void Initialize();
+	void Initialize( const unsigned int xSize = 10, const unsigned ySize = 50 );
+	void CreateWindow( const unsigned int xSize = 800, const unsigned ySize = 600 );
+	void ShowGrid();
 	void MainLoop();
 
 private:
+	void m_SetGameSize( const unsigned int xSize, const unsigned ySize );
+	void m_SetMainGridBlockBackgroundImage();
 	CGame();
 	
 	bool m_roundInProgress;
+	CMainGrid mainGrid;
 
 	static CGame* s_instance;
 
--- Tetris/src/Game.cpp
+++ Tetris/src/Game.cpp
@@ -1,4 +1,5 @@
 #include "Game.h"
+#include "FLTKWrapper.h"
 #include <cstddef>
 
 CGame::CGame()
@@ -21,14 +22,37 @@ void CGame::Destroy()
 
 }
 
-void CGame::Initialize()
+void CGame::Initialize( const unsigned int xSize, const unsigned ySize )
 {
+	m_SetGameSize( xSize, ySize );
+	m_SetMainGridBlockBackgroundImage();
+}
 
+void CGame::CreateWindow( const unsigned int xSize, const unsigned ySize)
+{
+	FLTKWrapper::Instance()->InitWindow();
+}
+
+void CGame::ShowGrid()
+{
+	FLTKWrapper::Instance()->Display( mainGrid );
 }
 
 void CGame::MainLoop()
 {
+	FLTKWrapper::Instance()->ShowWindow();
+}
 
+void CGame::m_SetGameSize( const unsigned int xSize, const unsigned ySize )
+{
+	mainGrid.SetSize( xSize, ySize );
+}
+
+void CGame::m_SetMainGridBlockBackgroundImage()
+{
+	boost::filesystem::path picDir = boost::filesystem::current_path();
+	picDir = picDir.parent_path() / "pic" / "BackGroundBlock.bmp";
+	mainGrid.SetBackgroundPicture( picDir, 10, 10 );
 }
 
 CGame* CGame::s_instance = NULL;
