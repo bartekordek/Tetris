b8f05e17ebe7f6c2886531887f88cb30fef37f98
Kordek Bartlomiej <bartekordek@gmail.com>
2015-05-05 15:07:12 +0200

    Fltk management moved to wrapper class.

--- Tetris/main.cpp
+++ Tetris/main.cpp
@@ -1,17 +1,12 @@
-#include <FL/Fl.H>
-#include <FL/Fl_Box.H>
-#include <FL/Fl_Window.H>
-
 #include "Game.h"
 
 int main( void )
 {
-	CGame::Instance()->Initialize();
-
-	Fl_Window window( 300, 300, "OKNO" );
-	Fl_Box box( 0, 0 , 300, 300, "haj wrld" );
-	window.show();
-	Fl::run();
+	const unsigned int initialXboardSize = 10;
+	const unsigned int initialYBoardSize = 50;
+	CGame::Instance()->Initialize( initialXboardSize, initialYBoardSize );
+	CGame::Instance()->CreateWindow();
+	CGame::Instance()->MainLoop();	
 	CGame::Instance()->Destroy();
 	return 0;
 }
