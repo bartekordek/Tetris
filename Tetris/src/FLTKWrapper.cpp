3a55b76b2ee9bcdd20197cda126cf9c98003eb9e
Kordek Bartlomiej <bartekordek@gmail.com>
2015-05-14 14:38:26 +0200

    Removed includes (they are already in header). Initialization of Fl_BMP_Image has been moved out of loop.

--- Tetris/src/FLTKWrapper.cpp
+++ Tetris/src/FLTKWrapper.cpp
@@ -1,9 +1,6 @@
 #include "FLTKWrapper.h"
 #include <cstddef>
-#include <FL/Fl.H>
 #include <FL/fl_draw.H>
-#include <FL/Fl_BMP_Image.H>
-
 
 
 FLTKWrapper::FLTKWrapper(): m_window( NULL )
@@ -24,11 +21,9 @@ FLTKWrapper* FLTKWrapper::Instance()
 	return s_instance;
 }
 
-
 void FLTKWrapper::InitWindow( const unsigned int xSize, const unsigned ySize )
 {
 	m_window = new Fl_Window( xSize, ySize, "Tetris" );
-	
 }
 
 void FLTKWrapper::ShowWindow()
@@ -40,31 +35,32 @@ void FLTKWrapper::Display( const CMainGrid& grid )
 {
 	const unsigned int rowsCount = grid.GetRowsCount();
 	const unsigned int colsCount = grid.GetColumnsCount();
-
 	const unsigned int slabCount = rowsCount*colsCount;
-
 	std::string gridImageLocation = grid.GetSlabPictureLoc( 0 );
-
 	const unsigned imgWidth = grid.GetImgWidth( 0, 0 );
 	const unsigned imgHeight = grid.GetImgHeight( 0, 0 );
+
+	Fl_BMP_Image* image = new Fl_BMP_Image( gridImageLocation.c_str() );
 
 	m_window->show();
 	for( unsigned int i = 0; i < slabCount; ++i )
 	{
-		DisplayImage( gridImageLocation, grid.GetSlabPositionX( i ) * 10, grid.GetSlabPositionY( i ) * 10, 10, 10 );
+		DisplayImage( image, grid.GetSlabPositionX( i ) * 10, grid.GetSlabPositionY( i ) * 10, 10, 10 );
 		// TODO Remove magic numbers (10, 10) - image size of bmps i made.
 	}
+	m_window->show();
 }
 
-void FLTKWrapper::DisplayImage( const std::string& imagePath,
-								const unsigned int xPos,
-								const unsigned int yPos,
-								const unsigned int width,
-								const unsigned int height )
+void FLTKWrapper::DisplayImage( 
+	Fl_BMP_Image* image,
+	const unsigned int xPos,
+	const unsigned int yPos,
+	const unsigned int width,
+	const unsigned int height )
 {
-	m_boxArray.push_back( new Fl_Box( xPos, yPos, 10, 10 ) );
-	Fl_BMP_Image image( imagePath.c_str() );
-	m_boxArray.back()->image( image );	
+	Fl_Box* box = new Fl_Box( xPos, yPos, 10, 10 );
+	box->image( image );
+	m_boxArray.push_back( box );
 }
 
 FLTKWrapper& FLTKWrapper::operator = ( const FLTKWrapper& wrapper )
