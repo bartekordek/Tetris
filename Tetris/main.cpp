6ae0e7c5a2b52ee7dc51064244be876f64248eb4
Kordek Bartlomiej <bartekordek@gmail.com>
2015-05-23 10:10:02 +0200

    Minor changes.

--- Tetris/main.cpp
+++ Tetris/main.cpp
@@ -5,9 +5,8 @@ int main( void )
 	const unsigned int columnsCount = 10;
 	const unsigned int rowsCount = 22;
 	CGame::Instance()->Initialize( rowsCount, columnsCount );
-	CGame::Instance()->CreateWindow();
+	CGame::Instance()->InitWindow();
 	CGame::Instance()->ShowGrid();
-	CGame::Instance()->MainLoop();	
 	CGame::Instance()->Destroy();
 	return 0;
 }
