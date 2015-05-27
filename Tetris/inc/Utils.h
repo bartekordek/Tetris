d55996dfe0ea13b1bf550ed6750f08f5814b4f10
bartekordek <bartekordek@gmail.com>
2015-05-27 12:55:55 +0200

    Added utlity with typedefs.

--- Tetris/inc/Utils.h
+++ Tetris/inc/Utils.h
@@ -3,6 +3,8 @@
 
 #include <vector>
 #include <boost/filesystem.hpp>
+#include <string>
+#include <FL/Fl_Box.H>
 #include "Orientation.h"
 
 namespace Fs = boost::filesystem;
@@ -10,9 +12,9 @@ namespace Fs = boost::filesystem;
 class CTableCoor
 {
 public:
-	CTableCoor( const unsigned row, const unsigned column );
-	const unsigned GetRow()const;
-	const unsigned GetCol()const;
+	CTableCoor( const unsigned int row, const unsigned int column );
+	const unsigned int GetRow()const;
+	const unsigned int GetCol()const;
 private:
 	unsigned m_row;
 	unsigned m_column;
@@ -21,8 +23,13 @@ private:
 typedef std::vector<CTableCoor> CoordinatestList;
 typedef Fs::path Path;
 
-typedef std::string string;
+typedef std::string String;
 
 typedef COrientation::Direction Direction;
+
+typedef std::vector<Fl_Box*> BoxVec;
+typedef const unsigned int CUInt;
+typedef const signed int CIntg;
+typedef unsigned int UInt;
 
 #endif
