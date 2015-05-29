c432b86a5b5f52cdad4e3ff4eebf96db9a70bf63
bartekordek <bartekordek@gmail.com>
2015-05-29 12:58:40 +0200

    Cleaning - const unsigned replaced by typedefs (and std::string).

--- Tetris/inc/MainGrid.h
+++ Tetris/inc/MainGrid.h
@@ -4,38 +4,48 @@
 #include <vector>
 #include "Utils.h"
 #include "Slab.h"
+#include "Brick.h"
 #include "Picture.h"
 
 class CMainGrid
 {
 public:
-	CMainGrid( const unsigned int rowsCount = 0, 
-			   const unsigned int columnsCount = 0, 
-			   const unsigned int initialX = 0, 
-			   const unsigned int initialY = 0 );
+	CMainGrid( CUInt rowsCount = 0, CUInt columnsCount = 0, CUInt initialX = 0, CUInt initialY = 0 );
 	virtual ~CMainGrid();
-	void SetSize( const unsigned int rowsCount,
-				  const unsigned int columnsCount,
-				  const unsigned int initialX = 0,
-				  const unsigned int initialY = 0 );
-	void SetBackgroundPicture( const boost::filesystem::path& picLocation,
-							   const unsigned int width, 
-							   const unsigned int height );
-	const unsigned int GetRowsCount()const;
-	const unsigned int GetColumnsCount()const;
-	const std::string GetSlabPictureLoc()const;
-	const unsigned int GetImgWidth()const;
-	const unsigned int GetImgHeight()const;
-	const unsigned int GetSlabRow( const unsigned int column, const unsigned int row )const;
-	const unsigned int GetSlabCol( const unsigned int column, const unsigned int row )const;
-	const unsigned int GetSlabRow( const unsigned int slabIndex )const;
-	const unsigned int GetSlabCol( const unsigned int slabIndex )const;
+	void SetSize( CUInt rowsCount, CUInt columnsCount, CUInt initialX = 0,  CUInt initialY = 0 );
+	void SetBackgroundPicture( const Path& picLocation, CUInt width, CUInt height );
+	void SetSlabPic( const Path& picLocation, CUInt width, CUInt height );
+	void AddBrick( const CBrick& brick );
+	void ReLeaseBrick();
+
+	CUInt GetRowsCount()const;
+	CUInt GetColumnsCount()const;
+	const String SlabPictureLoc()const;
+	const String EmptySlabPictureLoc()const;
+	CUInt GetImgWidth()const;
+	CUInt GetImgHeight()const;
+	CUInt GetSlabRow( CUInt slabIndex )const;
+	CUInt GetSlabCol( CUInt slabIndex )const;
+	const bool PartOfSlab( CUInt slabIndex )const;
+	const bool Empty( CUInt rowIndex, CUInt colIndex )const;
+	CUInt SlabCount()const;
+	const CoordinatestList ActiveBrickCoords()const;
+	const bool SlabExist( CUInt rowIndex, CUInt colIndex )const;
+	void AddCurrentBrickToGrid();
+	const bool PartOfCurrentBrick( CUInt rowIndex, CUInt colIndex )const;
+	void MoveActualBrick( const Direction direction );
+	const bool CheckIfBlockCanBeMoved( const Direction direction )const;
 
 private:
+	CUInt m_RowColToSlabIndex( CUInt rowIndex, CUInt colIndex )const;
+	
+	void m_SetToSlab( CUInt slabIndex );
+	CBrick* m_activeBrick;
 	std::vector<CSlab> m_slab;
-	unsigned int m_columnsCount;
-	unsigned int m_rowsCount;
+	UInt m_columnsCount;
+	UInt m_rowsCount;
 	CPicture m_slabBackground;
+	CPicture m_brickBckd;
 };
 
 #endif
--- Tetris/src/MainGrid.cpp
+++ Tetris/src/MainGrid.cpp
@@ -1,84 +1,237 @@
 #include "MainGrid.h"
+#include "BrickFactory.h"
 #include <boost/foreach.hpp>
 
-CMainGrid::CMainGrid( const unsigned int rowsCount, 
-					  const unsigned int columnsCount, 
-					  const unsigned int initialX, 
-					  const unsigned int initialY )
+CMainGrid::CMainGrid( CUInt rowsCount, 
+					  CUInt columnsCount, 
+					  CUInt initialX, 
+					  CUInt initialY )
 {
-	SetSize( rowsCount, columnsCount, initialX, initialY );
+	//SetSize( rowsCount, columnsCount, initialX, initialY );
 }
 
 CMainGrid::~CMainGrid()
 {
-
 }
 
-void CMainGrid::SetSize( const unsigned int rowsCount,
-						 const unsigned int columnsCount, 
-						 const unsigned int initialX, 
-						 const unsigned int initialY )
+void CMainGrid::SetSize( CUInt rowsCount,
+						 CUInt columnsCount, 
+						 CUInt initialX, 
+						 CUInt initialY )
 {
+	m_slab.erase( m_slab.begin(), m_slab.end() );
 	m_columnsCount = columnsCount;
 	m_rowsCount = rowsCount;
-	for( unsigned int i = 0; i < m_rowsCount; ++i )
+	for( UInt i = 0; i < m_rowsCount; ++i )
 	{
-		for( unsigned int j = 0; j < m_columnsCount; ++j )
+		for( UInt j = 0; j < m_columnsCount; ++j )
 		{
-			m_slab.push_back( CSlab( j + initialX, i + initialY ) );
+			CSlab slab( j + initialX, i + initialY, false, true );
+			m_slab.push_back( slab );
 		}
 	}
 }
 
-
-void CMainGrid::SetBackgroundPicture( const Path& picLocation, 
-									  const unsigned int width, 
-									  const unsigned int height )
+void CMainGrid::SetBackgroundPicture( const Path& picLocation, CUInt width, CUInt height )
 {
 	m_slabBackground = CPicture( picLocation, width, height );
 }
 
-const unsigned int CMainGrid::GetRowsCount()const
+void CMainGrid::SetSlabPic( const Path& picLocation, CUInt width, CUInt height )
+{
+	m_brickBckd = CPicture( picLocation, width, height );
+}
+
+void CMainGrid::ReLeaseBrick()
+{
+	m_activeBrick = CBrickFactory::GetRandomBrick();
+	AddBrick( *m_activeBrick );
+}
+
+void CMainGrid::AddBrick( const CBrick& brick )
+{
+	CoordinatestList& coords = brick.GetBlockPositions();
+	for( auto it = coords.begin(); it != coords.end(); ++it )
+	{
+		m_SetToSlab( m_RowColToSlabIndex( it->GetRow(), it->GetCol() ) );
+	}
+}
+
+void CMainGrid::AddCurrentBrickToGrid()
+{
+	if( NULL != m_activeBrick )
+	{
+		CoordinatestList coords = m_activeBrick->GetBlockPositions();
+		for( auto it = coords.begin(); it != coords.end(); ++it )
+		{
+			m_slab.at( m_RowColToSlabIndex( it->GetRow(), it->GetCol() ) ).Empty(false);
+		}
+		delete m_activeBrick;
+	}
+}
+
+void CMainGrid::m_SetToSlab( CUInt slabIndex )
+{
+	m_slab.at( slabIndex ).PartOfSlab( true );
+	m_slab.at( slabIndex ).Empty( false );
+}
+
+CUInt CMainGrid::GetRowsCount()const
 {
 	return m_rowsCount;
 }
 
-const unsigned int CMainGrid::GetColumnsCount()const
+CUInt CMainGrid::GetColumnsCount()const
 {
 	return m_columnsCount;
 }
 
-const std::string CMainGrid::GetSlabPictureLoc( )const
+const String CMainGrid::SlabPictureLoc()const
 {
 	return m_slabBackground.GetImgLoc();
 }
 
-const unsigned int CMainGrid::GetImgWidth()const
+const String CMainGrid::EmptySlabPictureLoc()const
+{
+	return m_brickBckd.GetImgLoc();
+}
+
+CUInt CMainGrid::GetImgWidth()const
 {
 	return 10;
 }
 
-const unsigned int CMainGrid::GetImgHeight()const
+CUInt CMainGrid::GetImgHeight()const
 {
 	return 10;
 }
 
-const unsigned int CMainGrid::GetSlabRow( const unsigned int column, const unsigned int row )const
-{
-	return GetSlabRow( row*column + column );
-}
-
-const unsigned int CMainGrid::GetSlabCol( const unsigned int column, const unsigned int row )const
-{
-	return GetSlabCol( row*column + column );
-}
-
-const unsigned int CMainGrid::GetSlabRow( const unsigned int slabIndex )const
+CUInt CMainGrid::GetSlabRow( CUInt slabIndex )const
 {
 	return m_slab.at( slabIndex ).Row();
 }
 
-const unsigned int CMainGrid::GetSlabCol( const unsigned int slabIndex )const
+CUInt CMainGrid::GetSlabCol( CUInt slabIndex )const
 {
 	return m_slab.at( slabIndex ).Col();
+}
+
+const bool CMainGrid::PartOfSlab( CUInt slabIndex )const
+{
+	return m_slab[slabIndex].PartOfSlab();
+}
+
+const bool CMainGrid::Empty( CUInt rowIndex, CUInt colIndex )const
+{
+	return m_slab.at( m_RowColToSlabIndex( rowIndex, colIndex ) ).Empty();
+}
+
+CUInt CMainGrid::SlabCount()const
+{
+	return GetRowsCount()*GetColumnsCount();
+}
+
+const CoordinatestList CMainGrid::ActiveBrickCoords()const
+{
+	CoordinatestList coords = m_activeBrick->GetBlockPositions();
+	return coords;
+}
+
+const bool CMainGrid::SlabExist( CUInt rowIndex, CUInt colIndex )const
+{
+	if( colIndex >= m_columnsCount || rowIndex >= m_rowsCount )
+	{
+		return false;
+	}
+	if( m_RowColToSlabIndex(rowIndex, colIndex ) < m_slab.size() )
+	{
+		return true;
+	}
+	return false;
+}
+
+CUInt CMainGrid::m_RowColToSlabIndex( CUInt rowIndex, CUInt colIndex )const
+{
+	return rowIndex*m_columnsCount + colIndex;
+}
+
+const bool CMainGrid::PartOfCurrentBrick( CUInt rowIndex, CUInt colIndex )const
+{
+	CoordinatestList coords = m_activeBrick->GetBlockPositions();
+	for( auto it = coords.begin(); it != coords.end(); ++it )
+	{
+		if( it->GetRow() == rowIndex && it->GetCol() == colIndex )
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+void CMainGrid::MoveActualBrick( const Direction direction )
+{
+	if( Direction::U == direction )
+	{
+
+	}
+	else if( Direction::D == direction )
+	{
+
+	}
+	else if( Direction::R == direction )
+	{
+
+	}
+	else if( Direction::L == direction )
+	{
+
+	}
+}
+
+const bool CMainGrid::CheckIfBlockCanBeMoved( const Direction direction )const
+{
+	CoordinatestList blockCoords = ActiveBrickCoords();
+	int RowDiff;
+	int ColDiff;
+	if( Direction::U == direction )
+	{
+		ColDiff = 0;
+		RowDiff = -1;
+	}
+	else if( Direction::D == direction )
+	{
+		ColDiff = 0;
+		RowDiff = 1;
+	}
+	else if( Direction::R == direction )
+	{
+		ColDiff = 1;
+		RowDiff = 0;
+	}
+	else if( Direction::L == direction )
+	{
+		ColDiff = -1;
+		RowDiff = 0;
+	}
+
+	for( auto it = blockCoords.begin(); it != blockCoords.end(); ++it )
+	{
+		CUInt actRow = it->GetRow();
+		CUInt actCol = it->GetCol();
+		if( false == SlabExist( actRow + RowDiff, actCol + ColDiff ) ) // Check if you are not in the bottom
+		{
+			return false;
+		}
+
+		if( true == PartOfCurrentBrick( actRow + RowDiff, actCol + ColDiff ) )
+		{
+			continue;
+		}
+
+		if( false == Empty( actRow + RowDiff, actCol + ColDiff ) )
+		{
+			return false;
+		}
+	}
+	return true;
 }
