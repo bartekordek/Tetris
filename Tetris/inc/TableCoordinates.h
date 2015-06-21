fb36c4e3316ac37ced990b237ff5e50adbcc1771
bartekordek <bartekordek@gmail.com>
2015-06-21 10:28:26 +0200

    TableCoordinates extracted as seperate files.

---
+++ Tetris/inc/TableCoordinates.h
@@ -0,0 +1,33 @@
+#ifndef _COORDS_
+#define _COORDS_
+
+#include "Utils.h"
+
+/*
+* @brief This class handles coordinates in table.
+* @details
+*/
+class CTableCoor
+{
+public:
+	CTableCoor( CUInt row, CUInt column );
+	CUInt Row()const;
+	CUInt Col()const;
+	void Row( CUInt row );
+	void Col( CUInt col );
+	/*
+	 * @brief Sets postion to row and col.
+	 * @param[in] const unsigned int - row number.
+	 * @param[in] const unsigned int - column number.
+	 */
+	void ChangePosition( CUInt row, CUInt col );
+	CTableCoor& operator=( const CTableCoor& coor );
+
+private:
+	unsigned m_row;
+	unsigned m_column;
+};
+
+typedef std::vector<CTableCoor> CoordinatestList;
+
+#endif
---
+++ Tetris/src/TableCoordinates.cpp
@@ -0,0 +1,41 @@
+#include "TableCoordinates.h"
+
+CTableCoor::CTableCoor( CUInt row, CUInt column ): m_row( row ), m_column( column )
+{
+}
+
+CTableCoor& CTableCoor::operator=( const CTableCoor& coor )
+{
+	if( this != &coor )
+	{
+		m_column = coor.m_column;
+		m_row = coor.m_row;
+	}
+	return *this;
+}
+
+CUInt CTableCoor::Col()const
+{
+	return m_column;
+}
+
+CUInt CTableCoor::Row()const
+{
+	return m_row;
+}
+
+void CTableCoor::Row( CUInt row )
+{
+	m_row = row;
+}
+
+void CTableCoor::Col( CUInt col )
+{
+	m_column = col;
+}
+
+void CTableCoor::ChangePosition( CUInt row, CUInt col )
+{
+	m_row = row;
+	m_column = col;
+}
