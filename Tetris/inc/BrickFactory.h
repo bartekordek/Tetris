e9d7cab214a715dc342ac7d6aede44bd6e373173
bartekordek <bartekordek@gmail.com>
2015-06-21 10:27:46 +0200

    Added comments.

--- Tetris/inc/BrickFactory.h
+++ Tetris/inc/BrickFactory.h
@@ -4,13 +4,20 @@
 #include "Brick.h"
 
 /*
- * @brief This class contains information about smallest part of game grid, which is slab. Slabs are parts of bricks.
- * @details
+ * @brief Mini utility class to generate specific type of brick.
+ * @details Can generate it randomly or by given enum item.
  */
 class CBrickFactory
 {
 public:
+	/*
+	 * @brief Generates Brick by given enum item.
+	 * @param[in] BrickTypes type of brick.
+	 */
 	static CBrick* GetBrick( const BrickTypes brickType );
+	/*
+	 * @brief Generates Brick randomly.
+	 */
 	static CBrick* GetRandomBrick();
 private:
 };
