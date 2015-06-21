75ef65f3be7c96160c39d3db758db4e96d53ce5d
bartekordek <bartekordek@gmail.com>
2015-06-21 10:30:12 +0200

    Removed table coordinates class from utils.

--- Tetris/inc/Utils.h
+++ Tetris/inc/Utils.h
@@ -7,31 +7,11 @@
 #include "Orientation.h"
 
 namespace Fs = boost::filesystem;
-
 typedef const unsigned int CUInt;
 typedef const signed int CIntg;
 typedef unsigned int UInt;
-
-class CTableCoor
-{
-public:
-	CTableCoor( CUInt row, CUInt column );
-	CUInt Row()const;
-	CUInt Col()const;
-	void Row( CUInt row );
-	void Col( CUInt col );
-	void ChangePosition( CUInt row, CUInt col );
-	CTableCoor& operator=( const CTableCoor& coor );
-
-private:
-	unsigned m_row;
-	unsigned m_column;
-};
-
-typedef std::vector<CTableCoor> CoordinatestList;
 typedef Fs::path Path;
-
 typedef std::string String;
-
 typedef COrientation::Direction Direction;
+
 #endif
