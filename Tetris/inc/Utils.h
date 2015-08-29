0f19f2fb5ff8a4c2e08f286fbe48572dc8a6a890
bartekordek <bartekordek@gmail.com>
2015-08-29 18:03:32 +0200

    Added typedef for std::thread.

--- Tetris/inc/Utils.h
+++ Tetris/inc/Utils.h
@@ -5,6 +5,7 @@
 #include <boost/filesystem.hpp>
 #include <string>
 #include "Orientation.h"
+#include <thread>
 
 namespace Fs = boost::filesystem;
 typedef const unsigned int CUInt;
@@ -13,6 +14,7 @@ typedef unsigned int UInt;
 typedef Fs::path Path;
 typedef std::string String;
 typedef COrientation::Direction Direction;
+typedef std::thread Thread;
 
 template<typename T>
 using Vector = std::vector < T > ;
