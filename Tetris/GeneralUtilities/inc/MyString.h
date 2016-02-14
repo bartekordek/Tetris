e86c23dfc441768d80410c8ece4a5872c8ffca78
bartekordek <bartekordek@gmail.com>
2016-02-14 13:18:16 +0100

    MyString is now a drived class.

--- Tetris/GeneralUtilities/inc/MyString.h
+++ Tetris/GeneralUtilities/inc/MyString.h
@@ -2,7 +2,8 @@
 
 #include <string>
 
-class String: std::string
+class String: 
+	public std::string
 {
 public:
 	String( void );
@@ -11,30 +12,8 @@ public:
 	String( const std::string& inputString );
 	virtual ~String();
 
-	String& operator=( const std::string& right );
-	String& operator=( const String& right );
-	const bool operator==( const std::string& right )const;
-	const bool operator==( const String& right )const;
-
-	const std::string& string()const;
-	const char* c_str()const;
-
-	const int Length()const;
-
-	String& Replace( const String& inWhat, const String& inFor );
-
-	const int rfind( const char* characters )const;
-	const int rfind( const char character )const;
-
-	const bool empty()const;
-
 	const bool Contains( const String& inputString )const;
 	const bool Contains( const char* inputString )const;
-	const int find( const String& inputString )const;
-	const int find( const char* inputString )const;
 
-	static int npos;
-
-private:
-	
+	String& Replace( const String& inWhat, const String& inFor );
 };
--- Tetris/GeneralUtilities/src/MyString.cpp
+++ Tetris/GeneralUtilities/src/MyString.cpp
@@ -1,18 +1,18 @@
 #include "MyString.h"
 
-String::String(void): mString( "" )
+String::String(void): std::string( "" )
 {
 }
 
-String::String( const char* inputString ):mString( inputString )
+String::String( const char* inputString ):std::string( inputString )
 {
 }
 
-String::String( const String& inputString ):mString( inputString.string() )
+String::String( const String& inputString ):std::string( inputString )
 {
 }
 
-String::String( const std::string& inputString ):mString( inputString )
+String::String( const std::string& inputString ):std::string( inputString )
 {
 
 }
@@ -21,104 +21,27 @@ String::~String()
 {
 }
 
-String& String::operator=( const std::string& right )
-{
-	if( mString != right )
-	{
-		mString = right;
-	}
-	return *this;
-}
-
-String& String::operator=( const String& right )
-{
-	if( this != &right )
-	{
-		mString = right.string();
-	}
-	return *this;
-}
-
-const bool String::operator==( const std::string& right )const
-{
-	if( 0 == mString.compare( right ) )
-	{
-		return true;
-	}
-	return false;
-}
-
-const bool String::operator==( const String& right )const
-{
-	if( 0 == mString.compare( right.string() ) )
-	{
-		return true;
-	}
-	return false;
-}
-
-const std::string& String::string()const
-{
-	return mString;
-}
 
 const bool String::Contains( const String& inputString )const
 {
 	return Contains( inputString.c_str() );
 }
 
-const char* String::c_str()const
-{
-	return mString.c_str();
-}
-
 const bool String::Contains( const char* inputString )const
 {
-	if( std::string::npos == mString.find( inputString ) )
+	if( std::string::npos == std::string::find( inputString ) )
 	{
 		return false;
 	}
 	return true;
 }
 
-const int String::find( const String& inputString )const
-{
-	return mString.find( inputString.c_str() );
-}
-
-const int String::find( const char* inputString )const
-{
-	return mString.find( inputString );
-}
-
 String& String::Replace( const String& inWhat, const String& inFor )
 {
-	auto inWhatPosition = mString.find( inWhat.c_str() );
+	auto inWhatPosition = std::string::find( inWhat.c_str() );
 	if( std::string::npos != inWhatPosition )
 	{
-		mString.replace( inWhatPosition, inWhat.Length(), inFor.c_str() );
+		std::string::replace( inWhatPosition, inWhat.length(), inFor.c_str() );
 	}
 	return *this;
 }
-
-const int String::rfind( const char* characters )const
-{
-	return mString.rfind( characters );
-}
-
-const int String::rfind( const char character )const
-{
-	return mString.rfind( character );
-}
-
-const bool String::empty()const
-{
-	return mString.empty();
-}
-
-const int String::Length()const
-{
-	return mString.length();
-}
-
-int String::npos = -1;
