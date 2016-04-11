2601ea8c4287083a3f7a23f80a2dba3a283b3832
bartekordek <bartekordek@gmail.com>
2016-04-11 19:41:41 +0200

    Added tests for named, position and size.

--- Tetris/Tests/src/MogeGenericTests.cpp
+++ Tetris/Tests/src/MogeGenericTests.cpp
@@ -1,6 +1,8 @@
 #include "MogeGenericTests.h"
 
 #include "Named.h"
+#include "Position.h"
+#include "Size.h"
 
 TEST_F( MogeGenericTests, NamedSetGetName )
 {
@@ -9,9 +11,70 @@ TEST_F( MogeGenericTests, NamedSetGetName )
 
 	};
 
-
 	EmptyClass instance;
 	const String genericName = "GenericName";
 	instance.SetName( genericName );
 	ASSERT_EQ( instance.GetName(), genericName );
+}
+
+TEST_F( MogeGenericTests, PositionSetGetTwoArgumentMethod )
+{
+	class EmptyClass: public MOGE::Position
+	{
+
+	};
+
+	EmptyClass instance;
+	CUInt x = 0;
+	CUInt y = 0;
+	instance.Set( x, y );
+	ASSERT_EQ( instance.GetX(), x );
+	ASSERT_EQ( instance.GetY(), y );
+}
+
+TEST_F( MogeGenericTests, PositionSetGetOneArgumentMethod )
+{
+	class EmptyClass: public MOGE::Position
+	{
+
+	};
+
+	EmptyClass instance;
+	CUInt x = 0;
+	CUInt y = 0;
+	instance.SetX( x );
+	instance.SetY( y );
+	ASSERT_EQ( instance.GetX(), x );
+	ASSERT_EQ( instance.GetY(), y );
+}
+
+TEST_F( MogeGenericTests, SizeSetGetTwoArgumentMethod )
+{
+	class EmptyClass: public MOGE::Size
+	{
+
+	};
+
+	EmptyClass instance;
+	CUInt width = 10;
+	CUInt height = 10;
+	instance.Set( width, height );
+	ASSERT_EQ( instance.GetWidth(), width );
+	ASSERT_EQ( instance.GetHeight(), height );
+}
+
+TEST_F( MogeGenericTests, SizeSetGetOneArgumentMethod )
+{
+	class EmptyClass: public MOGE::Size
+	{
+
+	};
+
+	EmptyClass instance;
+	CUInt width = 10;
+	CUInt height = 10;
+	instance.SetWidth( width );
+	instance.SetHeight( height );
+	ASSERT_EQ( instance.GetWidth(), width );
+	ASSERT_EQ( instance.GetHeight(), height );
 }
