709410ac97fdf42518db17d043a30fa3150cfe23
bartekordek <bartekordek@gmail.com>
2016-05-03 19:50:16 +0200

    Added boost aliasses.

--- Tetris/MogeLib/inc/Aliases.h
+++ Tetris/MogeLib/inc/Aliases.h
@@ -3,3 +3,7 @@
 using CUInt = const unsigned int;
 using CInt = const signed int;
 using UInt = unsigned int;
+
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/path.hpp>
+using ErrorCode = boost::system::error_code;
