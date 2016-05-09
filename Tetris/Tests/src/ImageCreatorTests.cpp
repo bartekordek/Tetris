3473188e91d69c74d4c88cbb15fa4fc8b3b14f40
bartekordek <bartekordek@gmail.com>
2016-05-09 16:51:56 +0200

    Replaced ImageSurface with container call.

--- Tetris/Tests/src/ImageCreatorTests.cpp
+++ Tetris/Tests/src/ImageCreatorTests.cpp
@@ -4,6 +4,6 @@
 
 TEST_F( ImageCreatorTests, BmpLoadTest )
 {
-	MOGE::ImageSurface imageSurface = MOGE::ImageCreator::CreateSurfaceFromImage( genericWindowsImagePath );
+	std::shared_ptr<MOGE::SurfaceWrapper> imageSurface = MOGE::ImageCreator::CreateSurfaceFromImage( genericWindowsImagePath );
 	ASSERT_NE( nullptr, imageSurface.get() );
 }
