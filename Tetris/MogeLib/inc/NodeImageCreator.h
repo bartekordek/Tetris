9cbfc514c4807e7d6c1ffd6b273534c9a80e82b6
bartekordek <bartekordek@gmail.com>
2016-05-10 17:24:10 +0200

    Removed comments.

--- Tetris/MogeLib/inc/NodeImageCreator.h
+++ Tetris/MogeLib/inc/NodeImageCreator.h
@@ -22,18 +22,4 @@ namespace MOGE
 
 		static ImageTypes GetImageType( const Path& imagePath );
 	};
-	
-//
-//	class NodeImageFactory
-//	{
-//	public:
-//		static std::shared_ptr<SDL_Surface> CreateImage( const Path& imagePath );
-//		static std::shared_ptr<SDL_Surface> CreateScreen( const Size& size, CUInt bitsPerPixel = 32 );
-//		static std::shared_ptr<SDL_Surface> CreateImage( SDL_Surface* surface );
-//	private:
-//		static SDL_Surface* CreateSurface( const Path& imagePath );
-////		static const IMAGETYPE GuessImageType( const Path& imagePath );
-//	};
-//
-//	ImagePtr CreateSharedImgPtr( SDL_Surface* image );
 }
