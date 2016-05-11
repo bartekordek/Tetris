5705bff578520cede41208de428ec631a215ec0f
bartekordek <bartekordek@gmail.com>
2016-05-11 15:34:02 +0200

    - Added missing files.
    - Fixed tetris build.

--- Tetris/main.cpp
+++ Tetris/main.cpp
@@ -1,14 +1,14 @@
 #include "inc/Game.h"
 #include <boost/log/core.hpp>
 
+#include <ConsoleUtilities.h>
+
 int main( int argc, char *argv[] )
 {
-	boost::filesystem::path::imbue( std::locale( "C" ) );
-
-	PrintInputParameters( argc, argv );
+	MOGE::ConsoleUtilities::PrintInputParameters( argc, argv );
 	const unsigned int columnsCount = 10;
 	const unsigned int rowsCount = 22;
-	CGame game;
+	Tetris::CGame game;
 	game.Initialize( rowsCount, columnsCount );
 	game.ShowGrid();
 	game.StartGame();
