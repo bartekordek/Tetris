562c7a2a36e9cbe3e3a1b08b36338f7d00d3b9d5
bartekordek <bartekordek@gmail.com>
2016-05-12 19:25:18 +0200

    Fix: ScreenNode how to have a new variable with surface, also was needed to implement UpdateImplementation method which was pure virtual in base class.

--- Tetris/MogeLib/inc/ScreenNode.h
+++ Tetris/MogeLib/inc/ScreenNode.h
@@ -13,11 +13,15 @@ namespace MOGE
 		void Initialize();
 		SDL_Window* GetScreen()const;
 		SDL_Renderer* GetRenderer()const;
+		SDL_Surface* GetSdlSurface()const;
 		void DestroyScreen();
+	protected:
+		virtual void UpdateImplementation();
 
 	private:
 		void FreeContent() override;
 		SDL_Window* mainScreen = nullptr;
 		SDL_Renderer* mainRenderer = nullptr;
+		SDL_Surface* mainScreenSurface = nullptr;
 	};
 }
--- Tetris/MogeLib/src/ScreenNode.cpp
+++ Tetris/MogeLib/src/ScreenNode.cpp
@@ -15,7 +15,8 @@ namespace MOGE
 	void ScreenNode::Initialize()
 	{
 		mainScreen = SDL_CreateWindow( Name::GetName().c_str(), Position::GetX(), Position::GetY(), Size::GetWidth(), Size::GetHeight(), SDL_WINDOW_SHOWN );
-		mainRenderer = SDL_CreateRenderer( mainScreen, -1, SDL_RENDERER_ACCELERATED );
+	//	mainRenderer = SDL_CreateRenderer( mainScreen, -1, SDL_RENDERER_ACCELERATED );
+		mainScreenSurface = SDL_GetWindowSurface( mainScreen );
 	}
 
 	SDL_Window* ScreenNode::GetScreen()const
@@ -28,9 +29,19 @@ namespace MOGE
 		return mainRenderer;
 	}
 
+	SDL_Surface* ScreenNode::GetSdlSurface()const
+	{
+		return mainScreenSurface;
+	}
+
 	void ScreenNode::DestroyScreen()
 	{
 		FreeContent();
+	}
+
+	void ScreenNode::UpdateImplementation()
+	{
+
 	}
 
 	void ScreenNode::FreeContent()
