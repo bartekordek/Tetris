c5ef79ced9db55e80535da9838c09f625c4619c7
bartekordek <bartekordek@gmail.com>
2016-05-16 13:35:38 +0200

    Added new class - implementation for input notifier.

---
+++ Tetris/MogeLib/inc/KeyboardListener.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <thread>
+
+#include <SDL_keyboard.h>
+#include <SDL_keycode.h>
+#include <SDL_events.h>
+
+namespace MOGE
+{
+	class KeyboardObservable;
+
+	class KeyboardObserver
+	{
+	public:
+		KeyboardObserver();
+		~KeyboardObserver();
+		virtual void KeyEvent( SDL_Keycode keyCode, SDL_EventType eventType ) = 0;
+	};
+}
---
+++ Tetris/MogeLib/inc/KeyboardObservable.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <vector>
+
+#include <SDL_keyboard.h>
+#include <SDL_keycode.h>
+
+#include "KeyboardListener.h"
+
+namespace MOGE
+{
+	class KeyboardObservable
+	{
+	public:
+		KeyboardObservable();
+		~KeyboardObservable();
+		void Initialize();
+		void Register( KeyboardObserver* observer );
+
+	private:
+		void PoolThreadMethod();
+		void NotifyObservers();
+
+		bool mPoolKeyboardEvents = true;
+		std::thread mPoolKeyboardEventsThread;
+
+		std::vector<KeyboardObserver*> mObservers;
+	};
+}
