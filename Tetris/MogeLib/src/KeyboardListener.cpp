277efeea50caae341c3bd14bc136a1f7f60ad786
bartekordek <bartekordek@gmail.com>
2016-05-16 13:37:20 +0200

    Added KeyboardListener class.

---
+++ Tetris/MogeLib/src/KeyboardListener.cpp
@@ -0,0 +1,18 @@
+#include "KeyboardListener.h"
+
+namespace MOGE
+{
+	KeyboardObserver::KeyboardObserver()
+	{
+	}
+
+
+	KeyboardObserver::~KeyboardObserver()
+	{
+	}
+
+	void KeyboardObserver::KeyEvent( SDL_Keycode keyCode, SDL_EventType eventType )
+	{
+
+	}
+}
---
+++ Tetris/MogeLib/src/KeyboardObservable.cpp
@@ -0,0 +1,45 @@
+#include "KeyboardObservable.h"
+#include <SDL_events.h>
+
+namespace MOGE
+{
+	KeyboardObservable::KeyboardObservable()
+	{
+	}
+
+
+	KeyboardObservable::~KeyboardObservable()
+	{
+		mPoolKeyboardEventsThread.join();
+	}
+
+	void KeyboardObservable::Initialize()
+	{
+		mPoolKeyboardEventsThread = std::thread( &KeyboardObservable::PoolThreadMethod, this );
+	}
+
+	void KeyboardObservable::Register( KeyboardObserver* observer )
+	{
+		mObservers.push_back( observer );
+	}
+
+	void KeyboardObservable::PoolThreadMethod()
+	{
+		SDL_Event event;
+		while( mPoolKeyboardEvents && SDL_PollEvent( &event ) )
+		{
+			if( SDL_KEYDOWN == event.type )
+			{
+				NotifyObservers();
+			}
+		}
+	}
+
+	void KeyboardObservable::NotifyObservers()
+	{
+		for( auto& observer: mObservers )
+		{
+		//	observer->KeyEvent();
+		}
+	}
+}
