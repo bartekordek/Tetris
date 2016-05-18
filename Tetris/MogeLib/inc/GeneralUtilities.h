bef84ac6d4e3058266ec62b817f145cc98ff6aac
bartekordek <bartekordek@gmail.com>
2016-05-18 16:49:02 +0200

    Removed commented code.

--- Tetris/MogeLib/inc/GeneralUtilities.h
+++ Tetris/MogeLib/inc/GeneralUtilities.h
@@ -5,53 +5,11 @@
 #include "TypesDeclarations.h"
 #include "Aliases.h"
 
-//#include "Singleton.h"
-//#include "FileSystem.h"
-
 #include <boost/filesystem.hpp>
 #include <boost/filesystem/path.hpp>
-
-
 
 #include <thread>
 
 #include <utility>
 #include <vector>
 #include <set>
-
-
-
-
-//using Mutex = std::mutex;
-//
-//
-//using CUInt = const unsigned int;
-//typedef const signed int CInt;
-//typedef unsigned int UInt;
-//typedef boost::filesystem::path FsPath;
-//
-//using Path = FS::Path;
-//
-//using Thread = std::thread;
-//
-//template<typename T>
-//using Vector = std::vector < T > ;
-//
-//template<typename KEY, typename TYPE>
-//using UnorderedMap = std::unordered_map<KEY, TYPE>;
-//
-//
-//template <typename TYPE>
-//using Set = std::set<TYPE>;
-//
-//template<typename T, typename Y>
-//using Pair = std::pair < T, Y > ;
-//
-//void PrintInputParameters( UInt argc, char* argv[] );
-//
-//using ErrorCode = boost::system::error_code;
-//
-//const bool FileExists( const Path& fileLocation, ErrorCode& errorCode );
-//
-//
-//
