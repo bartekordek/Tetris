a4275db98ebefc6929bd9c6a7c4ca225d70b9263
bartekordek <bartekordek@gmail.com>
2016-05-18 16:48:02 +0200

    Removed unused code and comments.

--- Tetris/MogeLib/src/GeneralUtilities.cpp
+++ Tetris/MogeLib/src/GeneralUtilities.cpp
@@ -1,15 +1,1 @@
 #include "GeneralUtilities.h"
-#include <iostream>
-
-void PrintInputParameters( UInt argc, char* argv[] )
-{
-	while( argc-- )
-	{
-		std::cout << "Argument " << argc << ": " << argv[argc] << std::endl;
-	}
-}
-
-//const bool FileExists( const Path& fileLocation, ErrorCode& errorCode )
-//{
-//	return boost::filesystem::is_regular_file( FsPath( fileLocation.c_str() ), errorCode );
-//}
