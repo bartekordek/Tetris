9707cbc738700ecf210c1b9f2c1c14b10880548d
Bartłomiej Kordek <bartekordek@gmail.com>
2016-09-23 12:01:27 +0200

    Uncommented code.

--- Tetris/MogeLib/src/ObjectNode.cpp
+++ Tetris/MogeLib/src/ObjectNode.cpp
@@ -23,82 +23,78 @@ namespace Moge
 	}
 	const int ObjectNodeContent::getZ()const
 	{
-		//this->position.getValue( Math::Axes::Z );
-		return 0;
+		return this->position.getValue( Math::Axes::Z );
 	}
 
 	void ObjectNodeContent::setXyz( const int x, const int y, const int z )
 	{
-		/*this->position.setValue( Math::Axes::X, x );
+		this->position.setValue( Math::Axes::X, x );
 		this->position.setValue( Math::Axes::Y, y );
-		this->position.setValue( Math::Axes::Z, z );*/
+		this->position.setValue( Math::Axes::Z, z );
 	}
 
 	void ObjectNodeContent::setXyz( const IPosition& size )
 	{
-		/*this->position.setValue( Math::Axes::X, size.getX() );
+		this->position.setValue( Math::Axes::X, size.getX() );
 		this->position.setValue( Math::Axes::Y, size.getY() );
-		this->position.setValue( Math::Axes::Z, size.getZ() );*/
+		this->position.setValue( Math::Axes::Z, size.getZ() );
 	}
 
 	void ObjectNodeContent::setX( const int x )
 	{
-		//this->position.setValue( Math::Axes::X, x );
+		this->position.setValue( Math::Axes::X, x );
 	}
 	void ObjectNodeContent::setY( const int y )
 	{
-		//this->position.setValue( Math::Axes::Y, y );
+		this->position.setValue( Math::Axes::Y, y );
 	}
 	void ObjectNodeContent::setZ( const int z )
 	{
-		//this->position.setValue( Math::Axes::Z, z );
+		this->position.setValue( Math::Axes::Z, z );
 	}
 
 	void ObjectNodeContent::setSize( const unsigned int width, const unsigned int height, const unsigned int depth )
 	{
-		/*this->position.setValue( Math::Axes::X, width );
-		this->position.setValue( Math::Axes::Y, height );
-		this->position.setValue( Math::Axes::Z, depth );*/
+		this->size.setValue( Math::Axes::X, width );
+		this->size.setValue( Math::Axes::Y, height );
+		this->size.setValue( Math::Axes::Z, depth );
 	}
 
 	void ObjectNodeContent::setSize( const ISize& size )
 	{
-		//this->position.setValue( Math::Axes::X, size.getWidth() );
-		//this->position.setValue( Math::Axes::Y, size.getHeight() );
-		//this->position.setValue( Math::Axes::Z, size.getDepth() );
+		this->size.setValue( Math::Axes::X, size.getWidth() );
+		this->size.setValue( Math::Axes::Y, size.getHeight() );
+		this->size.setValue( Math::Axes::Z, size.getDepth() );
 	}
 
 	void ObjectNodeContent::setWidth( const unsigned int width )
 	{
-		//this->position.setValue( Math::Axes::X, width );
+		this->size.setValue( Math::Axes::X, width );
 	}
 
 	void ObjectNodeContent::setHeight( const unsigned int height )
 	{
-		//this->position.setValue( Math::Axes::Y, height );
+		this->size.setValue( Math::Axes::Y, height );
 	}
 
 	void ObjectNodeContent::setHepth( const unsigned int depth )
 	{
-		//this->position.setValue( Math::Axes::Z, depth );
+		this->size.setValue( Math::Axes::Z, depth );
 	}
 
 	const unsigned int ObjectNodeContent::getWidth()const
 	{
-	//	return this->size.getValue( Math::Axes::X );
-		return 0;
+		return this->size.getValue( Math::Axes::X );
 	}
 
 	const unsigned int ObjectNodeContent::getHeight()const
 	{
-		//return this->size.getValue( Math::Axes::Y );
-		return 0;
+		return this->size.getValue( Math::Axes::Y );
 	}
 
 	const unsigned int ObjectNodeContent::getDepth()const
 	{
-		//return this->size.getValue( Math::Axes::Z );
-		return 0;
+		return this->size.getValue( Math::Axes::Z );
 	}
 
 	ObjectNodeContent::~ObjectNodeContent()
@@ -119,8 +115,8 @@ namespace Moge
 	void ObjectNodeContent::SetSurface( const ImageSurface& surface )
 	{
 		this->surface = surface;
-		//ISize::setWidth( surface->GetSdlSurface()->w );
-		//ISize::setHeight( surface->GetSdlSurface()->h );
+		setWidth( surface->GetSdlSurface()->w );
+		setHeight( surface->GetSdlSurface()->h );
 		Node::UpdateGeometrics();
 	}
 
@@ -128,8 +124,8 @@ namespace Moge
 	{
 		if( surface && surface->GetSdlSurface() )
 		{
-	/*		ISize::setWidth( surface->GetSdlSurface()->w );
-			ISize::setHeight( surface->GetSdlSurface()->h );*/
+			setWidth( surface->GetSdlSurface()->w );
+			setHeight( surface->GetSdlSurface()->h );
 			Node::UpdateGeometrics();
 		}
 	}
