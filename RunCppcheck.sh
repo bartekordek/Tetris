a093e09a31db0ed0158b33a4c2155fce1dd0b142
Bartłomiej Kordek <bartekordek@gmail.com>
2016-09-26 15:31:56 +0200

    Changed ROOTDIR, added tests.

--- RunCppcheck.sh
+++ RunCppcheck.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -xe
-ROOTDIR="Tetris"
-MOGELIB_ROOT="-I $ROOTDIR/MogeLib/inc "
-MOGELIB_RO="-I $ROOTDIR/MogeLib/RenderableObject/inc -I $ROOTDIR/MogeLib/RenderableObject/src"
-MOGELIB_SCENE="-I $ROOTDIR/MogeLib/Scene/inc"
-MOGELIB_STRING="-I $ROOTDIR/String/inc"
-MOGEMATH_ROOT="-I $ROOTDIR/MogeMath/inc"
-TETRIS_ROOT="-I $ROOTDIR/inc"
-INCLUDES="$MOGELIB_ROOT $MOGELIB_RO $MOGELIB_SCENE $MOGELIB_STRING $MOGEMATH_ROOT $TETRIS_ROOT" 
+ROOTDIR=`pwd`
+MOGELIB_ROOT="-I MogeLib/inc "
+MOGELIB_RO="-I MogeLib/RenderableObject/inc -I MogeLib/RenderableObject/src "
+MOGELIB_SCENE="-I MogeLib/Scene/inc "
+MOGELIB_STRING="-I String/inc "
+MOGEMATH_ROOT="-I MogeMath/inc "
+TESTS="-I Tests/inc/"
+TETRIS_ROOT="-I inc"
+INCLUDES="$MOGELIB_ROOT $MOGELIB_RO $MOGELIB_SCENE $MOGELIB_STRING $MOGEMATH_ROOT $TETRIS_ROOT $TESTS" 
 PREPROCESOR='-D MogeLib_EXPORT=True'
 SETTINGS="--enable=all --std=c++11 --suppress=missingIncludeSystem"
-cppcheck --check-config $SETTINGS $PREPROCESOR  "$ROOTDIR" $INCLUDES &> "CppCheckConfig.log"
-cppcheck $SETTINGS $PREPROCESOR "$ROOTDIR" $INCLUDES 2> StaticAnalysis.txt
+cppcheck "$ROOTDIR" --check-config $SETTINGS $PREPROCESOR $INCLUDES 2> "CppCheckConfig.log"
+cppcheck "$ROOTDIR" $SETTINGS $PREPROCESOR $INCLUDES 2> StaticAnalysis.txt
