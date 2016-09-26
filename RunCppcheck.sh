decf9fa9c47af10f6d55f291917f781eaaa9d3f3
Bartłomiej Kordek <bartekordek@gmail.com>
2016-09-26 11:33:45 +0200

    Updated cppcheck analysis tool.

--- RunCppcheck.sh
+++ RunCppcheck.sh
@@ -1,7 +1,13 @@
 #!/bin/bash -xe
 ROOTDIR="Tetris"
-INCLUDES="-I $ROOTDIR/MogeLib/inc -I Tetris/inc"
-PREPROCESOR='-D MOGE_API'
+MOGELIB_ROOT="-I $ROOTDIR/MogeLib/inc "
+MOGELIB_RO="-I $ROOTDIR/MogeLib/RenderableObject/inc -I $ROOTDIR/MogeLib/RenderableObject/src"
+MOGELIB_SCENE="-I $ROOTDIR/MogeLib/Scene/inc"
+MOGELIB_STRING="-I $ROOTDIR/MogeLib/String/inc"
+MOGEMATH_ROOT="-I $ROOTDIR/MogeMath/inc"
+TETRIS_ROOT="-I $ROOTDIR/inc"
+INCLUDES="$MOGELIB_ROOT $MOGELIB_RO $MOGELIB_SCENE $MOGELIB_STRING $MOGEMATH_ROOT $TETRIS_ROOT" 
+PREPROCESOR='-D MogeLib_EXPORT=True'
 SETTINGS="--enable=all --std=c++11 --suppress=missingIncludeSystem"
 cppcheck --check-config $SETTINGS $PREPROCESOR  "$ROOTDIR" $INCLUDES &> "CppCheckConfig.log"
 cppcheck $SETTINGS $PREPROCESOR "$ROOTDIR" $INCLUDES 2> StaticAnalysis.txt
