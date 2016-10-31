650db4b6f7126ef8fb76165b2fac4ea99a138e11
Bartłomiej Kordek <bartekordek@gmail.com>
2016-10-31 17:58:36 +0100

    Removed indent.

--- Tetris/src/Slab.cpp
+++ Tetris/src/Slab.cpp
@@ -2,80 +2,80 @@
 
 namespace Tetris
 {
-	Slab::Slab( CUInt row, CUInt col, const bool partOfSlab, const bool empty ):
-		position( CTableCoor( row, col ) ),
-		empty( empty )
-	{
-	}
+Slab::Slab( CUInt row, CUInt col, const bool partOfSlab, const bool empty ):
+	position( CTableCoor( row, col ) ),
+	empty( empty )
+{
+}
 
-	Slab::Slab( const Slab& slab ):
-		position( slab.position ),
-		empty( slab.empty )
-	{
-	}
+Slab::Slab( const Slab& slab ):
+	position( slab.position ),
+	empty( slab.empty )
+{
+}
 
-	Slab::~Slab()
-	{
-	}
+Slab::~Slab()
+{
+}
 
-	Slab& Slab::operator=( const Slab& slab )
+Slab& Slab::operator=( const Slab& slab )
+{
+	if( this != &slab )
 	{
-		if( this != &slab )
-		{
-			this->position = slab.position;
-			this->empty = slab.empty;
-			this->node = slab.node;
-		}
-		return *this;
+		this->position = slab.position;
+		this->empty = slab.empty;
+		this->node = slab.node;
 	}
+	return *this;
+}
 
-	CUInt Slab::Row()const
-	{
-		return position.Row();
-	}
+CUInt Slab::Row()const
+{
+	return position.Row();
+}
 
-	void Slab::Row( CUInt row )
-	{
-		position.Row( row );
-	}
+void Slab::Row( CUInt row )
+{
+	position.Row( row );
+}
 
-	void Slab::Col( CUInt col )
-	{
-		position.Col( col );
-	}
+void Slab::Col( CUInt col )
+{
+	position.Col( col );
+}
 
-	CUInt Slab::Col()const
-	{
-		return position.Col();
-	}
+CUInt Slab::Col()const
+{
+	return position.Col();
+}
 
-	void Slab::SetPosition( CUInt row, CUInt col )
-	{
-		this->position.ChangePosition( row, col );
-	}
+void Slab::SetPosition( CUInt row, CUInt col )
+{
+	this->position.ChangePosition( row, col );
+}
 
-	const bool Slab::Empty()const
-	{
-		return this->empty;
-	}
+const bool Slab::Empty()const
+{
+	return this->empty;
+}
 
-	void Slab::Empty( const bool empty )
-	{
-		this->empty = empty;
-	}
+void Slab::Empty( const bool empty )
+{
+	this->empty = empty;
+}
 
-	void Slab::SetId( CUInt id )
-	{
-		this->index = id;
-	}
+void Slab::SetId( CUInt id )
+{
+	this->index = id;
+}
 
-	void Slab::SetNode( const Moge::ObjectNode& node )
-	{
-		this->node = node;
-	}
+void Slab::SetNode( const Moge::ObjectNode& node )
+{
+	this->node = node;
+}
 
-	const Moge::ObjectNode& Slab::GetNode()const
-	{
-		return this->node;
-	}
+const Moge::ObjectNode& Slab::GetNode()const
+{
+	return this->node;
+}
 }
