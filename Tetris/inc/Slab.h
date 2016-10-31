51b27058402bbf2d88aaee9e6b8ee680f4ab0bb3
Bartłomiej Kordek <bartekordek@gmail.com>
2016-10-31 18:01:33 +0100

    Reduced indent.

--- Tetris/inc/Slab.h
+++ Tetris/inc/Slab.h
@@ -6,27 +6,27 @@
 
 namespace Tetris
 {
-	class Slab
-	{
-	public:
-		Slab( const Slab& slab );
-		Slab( CUInt row = 0, CUInt col = 0, const bool partOfSlab = false, const bool empty = true );
-		virtual ~Slab();
-		Slab& operator=( const Slab& slab );
-		CUInt Row()const;
-		CUInt Col()const;
-		void Row( CUInt row );
-		void Col( CUInt col );
-		void SetPosition( CUInt row, CUInt col );
-		const bool Empty()const;
-		void Empty( const bool empty );
-		void SetId( CUInt id );
-		void SetNode( const Moge::ObjectNode& node );
-		const Moge::ObjectNode& GetNode()const;
+class Slab
+{
+public:
+	Slab( const Slab& slab );
+	Slab( CUInt row = 0, CUInt col = 0, const bool partOfSlab = false, const bool empty = true );
+	virtual ~Slab();
+	Slab& operator=( const Slab& slab );
+	CUInt Row()const;
+	CUInt Col()const;
+	void Row( CUInt row );
+	void Col( CUInt col );
+	void SetPosition( CUInt row, CUInt col );
+	const bool Empty()const;
+	void Empty( const bool empty );
+	void SetId( CUInt id );
+	void SetNode( const Moge::ObjectNode& node );
+	const Moge::ObjectNode& GetNode()const;
 
-	private:
-		CTableCoor position;
-		bool empty;
-		Moge::ObjectNode node;
-	};
+private:
+	CTableCoor position;
+	bool empty;
+	Moge::ObjectNode node;
+};
 }
