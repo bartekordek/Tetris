1f8bd7ddff0cc753ffc23da560441518a2c2f298
Bartłomiej Kordek <bartekordek@gmail.com>
2016-10-31 22:43:06 +0100

    Reduced indent.

--- MogeMath/inc/ISize.h
+++ MogeMath/inc/ISize.h
@@ -2,32 +2,32 @@
 
 namespace Moge
 {
-	namespace Math
+namespace Math
+{
+template <typename Type>
+class ISize
+{
+public:
+	ISize()
 	{
-		template <typename Type>
-		class ISize
-		{
-		public:
-			ISize()
-			{
-			}
-
-			virtual ~ISize()
-			{
-			}
-
-			virtual void setSize( const Type width, const Type height, const Type depth ) = 0;
-			virtual void setWidth( const Type width ) = 0;
-			virtual void setHeight( const Type height ) = 0;
-			virtual void setHepth( const Type depth ) = 0;
-			virtual void setSize( const ISize& size ) = 0;
-
-			virtual const Type getWidth()const = 0;
-			virtual const Type getHeight()const = 0;
-			virtual const Type getDepth()const = 0;
-
-		protected:
-		private:
-		};
 	}
+
+	virtual ~ISize()
+	{
+	}
+
+	virtual void setSize( const Type width, const Type height, const Type depth ) = 0;
+	virtual void setWidth( const Type width ) = 0;
+	virtual void setHeight( const Type height ) = 0;
+	virtual void setDepth( const Type depth ) = 0;
+	virtual void setSize( const ISize& size ) = 0;
+
+	virtual const Type getWidth()const = 0;
+	virtual const Type getHeight()const = 0;
+	virtual const Type getDepth()const = 0;
+
+protected:
+private:
+};
+}
 }
