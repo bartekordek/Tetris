66e668226cda8a847358905fe7b868c0ccc038a2
Bartłomiej Kordek <bartekordek@gmail.com>
2016-10-31 22:42:53 +0100

    Updated cppcheck analysis.

--- RunCppcheck.sh
+++ RunCppcheck.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -xe
 ROOTDIR=`pwd`
-MOGELIB_ROOT="-I MogeLib/inc "
-MOGELIB_RO="-I MogeLib/RenderableObject/inc -I MogeLib/RenderableObject/src "
-MOGELIB_SCENE="-I MogeLib/Scene/inc "
-MOGELIB_STRING="-I String/inc "
-MOGEMATH_ROOT="-I MogeMath/inc "
-TESTS="-I Tests/inc/"
-TETRIS_ROOT="-I Tetris/inc"
+MOGELIB_ROOT="-I MogeLib/inc -I MogeLib/src"
+MOGELIB_RO="-I MogeLib/RenderableObject/inc -I MogeLib/RenderableObject/src"
+MOGELIB_SCENE="-I MogeLib/Scene/inc -I MogeLib/Scene/src"
+MOGELIB_STRING="-I String/inc -I String/src"
+MOGEMATH_ROOT="-I MogeMath/inc -I MogeMath/src -I MogeMath/Point"
+TESTS="-I Tests/inc/ -I Tests/src/"
+TETRIS_ROOT="-I Tetris/inc -I Tetris/src"
 INCLUDES="$MOGELIB_ROOT $MOGELIB_RO $MOGELIB_SCENE $MOGELIB_STRING $MOGEMATH_ROOT $TETRIS_ROOT $TESTS" 
 PREPROCESOR='-D MogeLib_EXPORT=True'
 SETTINGS="--enable=all --std=c++11 --suppress=missingIncludeSystem"
