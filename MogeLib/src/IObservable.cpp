04b2da6b626add1041c339753442c98dcb0f4cc6
Bartłomiej Kordek <bartekordek@gmail.com>
2016-12-24 20:34:07 +0100

    Added IObservable interface.

---
+++ MogeLib/src/IObservable.cpp
@@ -0,0 +1,20 @@
+#include "IObservable.h"
+#include "IObservableImpl.h"
+
+namespace Moge
+{
+	IObservable::IObservable(): impl( new IObservableImpl() )
+	{
+	}
+
+	IObservable::~IObservable()
+	{
+		delete this->impl;
+	}
+
+	void IObservable::registerObserver( IObserver* observer )
+	{
+		
+	}
+
+}
