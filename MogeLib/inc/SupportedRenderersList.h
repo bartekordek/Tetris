0ec543ae27203b198e838fccdca776755e1ed83b
Bartłomiej Kordek <bartekordek@gmail.com>
2017-03-21 16:12:22 +0100

    Renderer update.

--- MogeLib/inc/SupportedRenderersList.h
+++ MogeLib/inc/SupportedRenderersList.h
@@ -2,8 +2,13 @@
 
 namespace Moge
 {
-	enum class SupportedRenderers: short
+	enum class Supported2DRenderers: short
 	{
 		R_SDL = 0
+	};
+	
+	enum class Supported3DRenderers: short
+	{
+		NONE = 0;
 	};
 }
