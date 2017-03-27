8672b7dadeb573d60b1b0439ece409444d106180
Bartłomiej Kordek <bartekordek@gmail.com>
2017-03-27 08:27:55 +0200

    Updated methods to match base class.

--- MogeLib/src/RenderableSDLTexture.cpp
+++ MogeLib/src/RenderableSDLTexture.cpp
@@ -1,30 +1,42 @@
 #include "RenderableSDLTexture.h"
-
+#include "Math/PositionDouble2D.h"
+#include "Math/SizeDouble2D.h"
 namespace Moge
 {
 	RenderableSDLTexture::RenderableSDLTexture()
 	{
-		
+		this->position.reset( new Math::PositionDouble2D() );
+		this->size.reset( new Math::SizeDouble2D() );
 	}
 	
-	RenderableSDLTexture::~RenderableSDLTexture()
+	Math::IPosition<double>* RenderableSDLTexture::getPosition()
 	{
-		
+		return this->position.get();
 	}
-	
-	const Math::IPosition<double>& RenderableSDLTexture::getPosition()const
+
+	const Math::IPosition<double>* RenderableSDLTexture::getPosition()const
 	{
-		
+		return this->position.get();
 	}
 	
-	const Math::ISize<double>& RenderableSDLTexture::getSize()const
+	Math::ISize<double>* RenderableSDLTexture::getSize()
 	{
-		
+		return this->size.get();
 	}
-	
-	const ITexture* RenderableSDLTexture::getTexture()const
+
+	const Math::ISize<double>* RenderableSDLTexture::getSize()const
 	{
-		
+		return this->size.get();
 	}
-	
+
+	const ITexture* RenderableSDLTexture::getTexture() const
+	{
+		return this->texture.get();
+	}
+
+	void RenderableSDLTexture::setTexture( const std::shared_ptr< ITexture >& texture )
+	{
+		this->texture = texture;
+	}
+
 }
--- MogeLib/src/RenderableSDLTexture.h
+++ MogeLib/src/RenderableSDLTexture.h
@@ -1,16 +1,25 @@
 #pragma once
 #include "IRenderable.h"
+#include "ITexture.h"
+#include <memory>
+
 namespace Moge
 {
 	class RenderableSDLTexture: public IRenderable
 	{
 	public:
 		RenderableSDLTexture();
-		virtual ~RenderableSDLTexture();
-		const Math::IPosition<double>& getPosition()const;
-		const Math::ISize<double>& getSize()const;
+		virtual ~RenderableSDLTexture() = default;
+		Math::IPosition<double>* getPosition()override;
+		const Math::IPosition<double>* getPosition()const override;
+		Math::ISize<double>* getSize() override;
+		const Math::ISize<double>* getSize()const override;
 		const ITexture* getTexture()const;
+		void setTexture( const std::shared_ptr<ITexture>& texture );
 	protected:
 	private:
+		std::shared_ptr<ITexture> texture;
+		std::unique_ptr<Math::IPosition<double>> position;
+		std::unique_ptr<Math::ISize<double>> size;
 	};
 }
