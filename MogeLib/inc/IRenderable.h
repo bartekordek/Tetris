ff8f3045a3a0ab29118004aa6160fe5c8bb62b8a
Bartłomiej Kordek <bartekordek@gmail.com>
2017-03-27 08:26:35 +0200

    Added getters for position.

--- MogeLib/inc/IRenderable.h
+++ MogeLib/inc/IRenderable.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #include "MogeLib.h"
+#include "RenderableTypes.h"
 #include "Math/IPosition.h"
-#include "ITexture.h"
 #include "Math/ISize.h"
 
 namespace Moge
@@ -12,10 +12,13 @@ namespace Moge
 	public:
 		IRenderable();
 		virtual ~IRenderable();
-		virtual const Math::IPosition<double>& getPosition()const = 0;
-		virtual const Math::ISize<double>& getSize()const = 0;
-		virtual void render() = 0;
+		virtual Math::IPosition<double>* getPosition() = 0;
+		virtual const Math::IPosition<double>* getPosition()const = 0;
+		virtual Math::ISize<double>* getSize() = 0;
+		virtual const Math::ISize<double>* getSize()const = 0;
+		virtual const RenderableType getRenderableType()const = 0;
 	protected:
 	private://TODO Write texture render method.
+		
 	};
 }
