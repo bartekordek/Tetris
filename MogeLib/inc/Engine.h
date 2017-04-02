77e0a01825768ba29aa358f32c95dedd6bdcaea1
Bartłomiej Kordek <bartekordek@gmail.com>
2017-04-02 23:00:13 +0200

    Removed renderable objects. Replaced ITextureFactory2D/3D with its base class. Style: spaces. Style: unused imports.

--- MogeLib/inc/Engine.h
+++ MogeLib/inc/Engine.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <atomic>
-#include <set>
 #include <map>
 #include <thread>
 #include <mutex>
@@ -13,18 +12,18 @@
 #include "ObjectNode.h"
 #include "IKeyboardObservable.h"
 #include "INodeFactory.h"
-#include "ITextureFactory.h"
 #include "IRenderer.h"
 #include "Math/ISize.h"
 #include "IRenderer2D.h"
 #include "IRenderer3D.h"
+#include "ITextureFactory.h"
 
 namespace Moge
 {
 	class IKeyboardObserver;
-    class IKeyboardData;
-    class IKey;
-    class IKeyFactory;
+	class IKeyboardData;
+	class IKey;
+	class IKeyFactory;
 	class MogeLib_API Engine: public Singleton<Engine>, public IKeyboardObservable
 	{
 	public:
@@ -38,18 +37,17 @@ namespace Moge
 		void createScreen( Math::ISize<unsigned int>& size, Math::IPosition<int>& position, const std::string& label = "Window label." );
 		void startMainLoop();
 		void stopEventLoop();
-		ITextureFactory2D* get2DTextureFactory()const;
-		ITextureFactory3D* get3DTextureFactory()const;
+		ITextureFactory* get2DTextureFactory()const;
+		ITextureFactory* get3DTextureFactory()const;
 
 	protected:
 
 	private:
 		void eventPool();
-		void renderingLoop();
+		void renderingLoop2D();
 		void QueueFrame();
 		void Render( Node& node );
 
-		std::set<std::shared_ptr<Node>> mRenderableObjects;
 		std::mutex mRenderableObjectsMutex;
 		std::mutex mListMutex;
 		std::thread mainLoop;
@@ -60,6 +58,7 @@ namespace Moge
 		const uint8_t* sdlKey = nullptr;
 		std::map<unsigned int, std::shared_ptr<IKey>> keys;
 		std::unique_ptr<IKeyFactory> keyFactory;
+
 		std::unique_ptr<INodeFactory> nodeFactory;
 		
 		std::unique_ptr<IRenderer2D> renderer2D;
