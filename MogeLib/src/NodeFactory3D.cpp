48d421f618b1a242ce3916adb436105741f900cf
Bartłomiej Kordek <bartekordek@gmail.com>
2017-04-11 10:09:28 +0200

    Style fixes. Changed reference to ptr.
    Replaced set with IList.

--- MogeLib/src/NodeFactory3D.cpp
+++ MogeLib/src/NodeFactory3D.cpp
@@ -1,51 +1,63 @@
 #include "NodeFactory3D.h"
+#include "ITextureFactory3D.h"
+#include "ListVector.h"
 namespace Moge
 {
-	NodeFactory3D::NodeFactory3D(  ITextureFactory3D* factory3D ): factory3D( factory3D )
+	NodeFactory3D::NodeFactory3D( ITextureFactory3D* factory3D ): 
+		factory3D( factory3D ),
+		nodes( new ListVector<Node>() )
 	{
 	}
 
 	using namespace Math;
-
 	std::shared_ptr<Node> NodeFactory3D::CreateFromImage(
 		const Path& filePath, 
 		const Math::IPosition<double>& position, 
 		const MyString& name )
 	{
 		std::shared_ptr<Node> result;
-		//std::shared_ptr<ITexture>& texture = this->
+		const auto& texture = this->factory3D->createTexture( filePath );
+		result->setTexture( texture );
+		result->SetName( name );
+
 		return result;
 	}
 
-	std::shared_ptr<Node> NodeFactory3D::createFromTexture(
-		const std::shared_ptr<ITexture>& texture, 
-		const Math::IPosition<double>& position , 
-		const MyString& name )
+	std::shared_ptr<Node> NodeFactory3D::createFromTexture(const std::shared_ptr<ITexture>& texture, const IPosition<double>& position , const MyString& name)
 	{
-		Node* objectNodeContent = new Node();
-		objectNodeContent->getPosition().setXyz( position );
-		return std::shared_ptr<Node>( objectNodeContent );
+		Node* node = new Node();
+		node->getPosition().setXyz( position );
+		node->setTexture( texture );
+		return std::shared_ptr<Node>( node );
 	}
 	
-	void NodeFactory3D::remove(const std::shared_ptr<Node>& node)
+	void NodeFactory3D::remove( const std::shared_ptr<Node>& node )
 	{
-		this->nodes.erase( node.get() );
+		const Node* nodePtr = static_cast<const Node*>( node.get() );
+		remove( nodePtr );
+	}
+
+	void NodeFactory3D::remove( const Node* node )
+	{
+		this->nodes->remove( *node );
 	}
 	
-	const bool NodeFactory3D::exist(const std::shared_ptr<Node>& node)const
+	const bool NodeFactory3D::exist( const std::shared_ptr<Node>& node )const
 	{
-		for( auto& currentNode : this->nodes )
-		{
-			if( currentNode == node.get() )
-			{
-				return true;
-			}
-		}
-		return false;
+		const std::shared_ptr<IIterator<Node>> ret = this->nodes->find( *node.get() );
+		const IIterator<Node>& it1 = *ret.get();
+		const IIterator<Node>& it2 = this->nodes->end();
+		return !(it1 == it2);
 	}
-	
+		
 	const unsigned int NodeFactory3D::count()const
 	{
-		return static_cast<unsigned int>( this->nodes.size() );
+		return static_cast<unsigned int>( this->nodes->size() );
 	}
+	
+	IIterator<Node>& NodeFactory3D::getNodes()
+	{
+		return this->nodes->getRandomIterator();
+	}
+	
 }
--- MogeLib/src/NodeFactory3D.h
+++ MogeLib/src/NodeFactory3D.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "INodeFactory.h"
-#include <set>
+#include "IList.h"
+#include <memory>
 namespace Moge
 {
 	class ITextureFactory3D;
@@ -18,12 +19,13 @@ namespace Moge
 				const Math::IPosition<double>& position = Math::IPositionAdapter<double>( 0.0, 0.0, 0.0 ),
 				const MyString& name = MyString( "" ) ) override;
 		void remove(const std::shared_ptr<Node>& node) override;
+		void remove( const Node* node ) override;
 		const bool exist(const std::shared_ptr<Node>& node )const override;
 		const unsigned int count()const override;
 		IIterator<Node>& getNodes()override;
 	protected:
 	private:
 		ITextureFactory3D* factory3D = nullptr;
-		std::set< Node* > nodes;
+		std::unique_ptr< IList<Node>> nodes;
 	};
 }
