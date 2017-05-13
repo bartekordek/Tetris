6615940adee3112ee5fc4feb6ab673015f2be192
Bartłomiej Kordek <bartekordek@gmail.com>
2017-05-13 19:54:22 +0200

    New class for thread-safe store of primitives.

---
+++ MogeLib/inc/LckPrim.h
@@ -0,0 +1,114 @@
+#pragma once
+#include "MogeLib.h"
+#include <mutex>
+namespace Moge
+{
+	template <typename Type>
+	class MogeLib_API LckPrim
+	{
+	public:
+		LckPrim( const Type& val )
+		{
+			std::lock_guard<std::mutex> lock( this->mtx );
+			this->value = val;
+		}
+
+		LckPrim( const LckPrim<Type>& rvalue )
+		{
+			std::lock_guard<std::mutex> lock( this->mtx );
+			this->value = rvalue.value;
+		}
+
+		virtual ~LckPrim()
+		{
+			
+		}
+
+		const Type& getVal()const
+		{
+			std::lock_guard<std::mutex> lock( this->mtx );
+			return this->value;
+		}
+
+		void setVal( const Type& val )
+		{
+			std::lock_guard<std::mutex> lock( this->mtx );
+			this->value = val;
+		}
+
+		LckPrim<Type>& operator=( const LckPrim<Type>& rvalue )
+		{
+			if( this != &rvalue )
+			{
+				std::lock_guard<std::mutex> lock( this->mtx );
+				this->value = rvalue.value;
+			}
+			return *this;
+		}
+
+		LckPrim<Type>& operator=( const Type& value )
+		{
+			std::lock_guard<std::mutex> lock( this->mtx );
+			this->value = value;
+			return *this;
+		}
+
+		Type& operator++()
+		{
+			std::lock_guard<std::mutex> lock( this->mtx );
+			++this->value;
+			return this->value;
+		}
+
+		Type operator++( int )
+		{
+			std::lock_guard<std::mutex> lock( this->mtx );
+			Type copy = this->value;
+			++this->value;
+			return copy;
+		}
+
+		Type& operator--()
+		{
+			std::lock_guard<std::mutex> lock( this->mtx );
+			--this->value;
+			return this->value;
+		}
+
+		Type operator--( int )
+		{
+			std::lock_guard<std::mutex> lock( this->mtx );
+			Type copy = this->value;
+			--this->value;
+			return copy;
+		}
+
+		const bool operator==( const LckPrim<Type>& val )
+		{
+			std::lock_guard<std::mutex> lock( this->mtx );
+			return this->value == val.value;
+		}
+
+		const bool operator==( const Type& val )
+		{
+			std::lock_guard<std::mutex> lock( this->mtx );
+			return this->value == val;
+		}
+
+		operator Type()
+		{
+			return this->value;
+		}
+
+	protected:
+	private:
+		std::mutex mtx;
+		Type value = static_cast<Type>( 0 );
+	};
+
+	template <typename Type>
+	const bool operator==( const Type& r, LckPrim<Type>&l )
+	{
+		return l == r;
+	}
+}
