4f799b9771ba56727fe74e01e47c351f2e44debd
Bartłomiej Kordek <bartekordek@gmail.com>
2018-02-16 15:40:05 +0100

    Netbeans project fixes.

---
+++ libmogeTests/libmogeTests/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=libmogetests
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/libmogetests
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=libmogetests.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/libmogetests.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=libmogetests
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/libmogetests
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=libmogetests.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/libmogetests.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
