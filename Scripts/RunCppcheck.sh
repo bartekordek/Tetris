98200b40bcf57f118b08686768d9fceb9ff5a2e1
Bartłomiej Kordek <bartekordek@gmail.com>
2018-05-31 16:47:00 +0200

    Updated StaticAnalysis.

--- Scripts/RunCppcheck.sh
+++ Scripts/RunCppcheck.sh
@@ -1,5 +1,6 @@
-#!/bin/bash -xe
+#!/bin/bash
 ROOT_DIR=..
+CPPCHECK_EXECUTABLE=$ROOT_DIR/tools/cppcheck/cppcheck.exe
 PREPROCESOR='-D MogeLib_EXPORT=""'
 SUPRESS="--suppress=information:SDL_surface.h --suppress=information:gtest/gtest.h"
 SETTINGS="--enable=all --std=c++11 --suppress=missingIncludeSystem $SUPRESS"
@@ -11,11 +12,10 @@ HEADERS=`for i in \`echo $HEADERS\`; do
 	echo $i
 done | sort -u`
 HEADERS=`echo $HEADERS | sed 's/\../-I ../g'` 
-echo "Runing cppcheck..."
 if [ "$1" == "--check-config" ];then
-	cppcheck $ROOTDIR $CPP_FILES $SETTINGS $PREPROCESOR $HEADERS --check-config 2> "CppCheckConfig.txt"
+	$CPPCHECK_EXECUTABLE $ROOTDIR $CPP_FILES $SETTINGS $PREPROCESOR $HEADERS --check-config 2> "CppCheckConfig.txt"
 else
-	cppcheck $ROOTDIR $CPP_FILES $SETTINGS $PREPROCESOR $HEADERS 2> StaticAnalysis.txt
+	$CPPCHECK_EXECUTABLE $ROOTDIR $CPP_FILES $SETTINGS $PREPROCESOR $HEADERS 2> StaticAnalysis.txt
 fi
 
 find $ROOT_DIR -name '*.h' > HeaderList.txt
